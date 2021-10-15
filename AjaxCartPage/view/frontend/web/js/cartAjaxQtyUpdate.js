29337a91b3232fefa79e1ef3b002705bef754ff1
leo <huy.hoquoc99@gmail.com>
2021-10-15 09:30:36 +0700

    Chapter 3 - Exercise 2: Ajax Cart Page

---
+++ AjaxCartPage/view/frontend/web/js/cartAjaxQtyUpdate.js
@@ -0,0 +1,68 @@
+define([
+    'jquery',
+    'Magento_Checkout/js/action/get-totals',
+    'Magento_Customer/js/customer-data'
+], function ($, getTotalsAction, customerData) {
+
+    $(document).ready(function(){
+        function ajaxSubmitForm() {
+            var form = $('form#form-validate');
+            $.ajax({
+                url: form.attr('action'),
+                data: form.serialize(),
+                showLoader: true,
+                success: function (res) {
+                    var parsedResponse = $.parseHTML(res);
+                    var result = $(parsedResponse).find("#form-validate");
+                    var sections = ['cart'];
+
+                    $("#form-validate").replaceWith(result);
+
+                    // The mini cart reloading
+                    customerData.reload(sections, true);
+
+                    // The totals summary block reloading
+                    var deferred = $.Deferred();
+                    getTotalsAction([], deferred);
+                },
+                error: function (xhr, status, error) {
+                    var err = eval("(" + xhr.responseText + ")");
+                    console.log(err.Message);
+                }
+            });
+        }
+
+        $(document).on('change', 'input[name$="[qty]"]', function() {
+            ajaxSubmitForm();
+        });
+
+        $(document).on('click', '.action.action-delete', function(e) {
+            e.preventDefault();
+            var removeButton = $('.action.action-delete').attr('data-post');
+            var dataPost = JSON.parse(removeButton);
+            $.ajax({
+                url: dataPost.action,
+                data: JSON.stringify(dataPost.data),
+                showLoader: true,
+                success: function (res) {
+                    var parsedResponse = $.parseHTML(res);
+                    var result = $(parsedResponse).find("#form-validate");
+                    var sections = ['cart'];
+
+                    $("#form-validate").replaceWith(result);
+
+                    // The mini cart reloading
+                    customerData.reload(sections, true);
+
+                    // The totals summary block reloading
+                    var deferred = $.Deferred();
+                    getTotalsAction([], deferred);
+                },
+                error: function (xhr, status, error) {
+                    var err = eval("(" + xhr.responseText + ")");
+                    console.log(err.Message);
+                }
+            });
+        });
+    });
+});
